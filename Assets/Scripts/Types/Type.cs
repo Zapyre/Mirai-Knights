7cd8480b9fb4c6016798e346e009682bdb3729ba
John Tai <zapyre_1@hotmail.com>
2015-03-04 14:29:01 -0500

    Setting up Repo commits and structure

---
+++ Assets/Scripts/Types/Type.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Type{
+	// Str Related Attributes
+	private int attack;
+	// Dex Related Attributes
+	// Int Related Attributes
+	// End Related Attributes
+	private int health;
+}
---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit{
+	private Texture unitPortrait;
+	private Type unitType;
+
+	private int strength;
+	private int dexterity;
+	private int intelligence;
+	private int endurance;
+
+	public Unit (int s, int d, int i, int e){
+		strength = s;
+		dexterity = d;
+		intelligence = i;
+		endurance = e;
+	}
+
+	public void SetUnitPortrait (Texture up) { unitPortrait = up; }
+	public void SetUnitType (Type ut) { unitType = ut; }
+
+	public int GetStrength () { return strength; }
+	public void SetStrength (int s) { strength = s; }
+	public int GetDexterity () { return dexterity; }
+	public void SetDexterity (int d) { dexterity = d; }
+	public int GetIntelligence () { return intelligence; }
+	public void SetIntelligence (int i) { intelligence = i; }
+	public int GetEndurance () { return endurance; }
+	public void SetEndurance (int e) { endurance = e; }
+
+
+}
